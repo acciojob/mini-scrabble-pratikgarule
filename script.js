cb8bda3e74edb20932b341982e46313ce31516c0
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-06-25 06:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let input = document.getElementById("evaluatedText");
+let count = document.getElementById("letterCount");
+
+
+
+input.addEventListener("keyup", updatecounter);
+function updatecounter() {
+    let str = input.value;
+    let c = str.length;
+    count.innerText = c;
+}
